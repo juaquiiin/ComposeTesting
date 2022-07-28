0a6af8bac3d896aac6426a2ad877ac4aecba3ae6
jcardenas <jocarur@gmail.com>
2022-07-27 20:30:18 -0500

    Un ejemplo de lambda

--- TestModule/src/main/java/com/jocarur/testmodule/TestStatements.kt
+++ TestModule/src/main/java/com/jocarur/testmodule/TestStatements.kt
@@ -1,8 +1,10 @@
 package com.jocarur.testmodule
 
 fun main(){
-    testLoop()
-    testWhen()
+    print(sum(12, 15))
+    println(sumV1(12, 15))
+//    testLoop()
+//    testWhen()
 }
 
 fun testLoop() {
@@ -17,7 +19,7 @@ fun testWhen() {
     when (0) {
         /**
          * Omg you can use in to define ranges in the code
-         * And you can use the reverse holy cow with !in
+         * And you can use the reverse with !in holy cow
          * Well that makes sense it's a bool
          */
         in 1..49 -> {
@@ -31,3 +33,11 @@ fun testWhen() {
         }
     }
 }
+
+/**
+ * It's a short function
+ * This is a lambda and the way we would write the function
+ */
+val sum: (Int, Int) -> Int = { a, b -> a + b }
+
+fun sumV1(a: Int, b: Int): Int = a + b
